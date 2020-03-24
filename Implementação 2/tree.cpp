fed0f8b9c2ce3f02f60bd83acdcdaeb261ff7fd5
Diego Luiz <51066402+Diego-Luiz@users.noreply.github.com>
2020-03-24 19:12:31 -0300

    Update tree.cpp

--- Implementação 2/tree.cpp
+++ Implementação 2/tree.cpp
@@ -10,21 +10,26 @@ class Node{
     vector<Node*> *filhos;  
     
     Node(int key, vector<Node*> *f){
-      this->key = key;
-      this->filhos = f;
+      this->key = key;  //chave 
+      this->filhos = f; //lista de nós em relação à aquela chave
     }
 };
 
-Node *root = NULL;
-bool flag = false;
+Node *root = NULL; //raiz da árvore
+bool flag = false; //variável usada para inserção
 
-void busca(Node *no, Node *pai, int chave){
+/*  ->Parametros
+    no: nó atual que esta sendo usado para as comparações
+    pai: nó que deseja ser inserido 
+    chave: valor do nó onde o pai deve ser inserido
+*/
+void adiciona(Node *no, Node *pai, int chave){
 
   if(no!=NULL){
     if(no->key == chave){
       if(no->filhos == NULL)
         no->filhos = new vector<Node*>;
-      
+
       no->filhos->push_back(pai);
       flag = true;
     }
@@ -33,13 +38,12 @@ void busca(Node *no, Node *pai, int chave){
       if(no->filhos != NULL){
         for(int i=0; i<no->filhos->size(); i++){
           if(!flag)
-            busca( (*(no->filhos))[i], pai, chave );
+            adiciona( (*(no->filhos))[i], pai, chave );
         }
-
       }
 
     }
-    
+
   }
  
 }
@@ -86,41 +90,68 @@ int main() {
   int N = 0; //chave
   
   while(true){
-    vector<Node*> *filhos = new vector<Node*>;
     string P = ""; //pai deste no (N)
     int F = 0; //filhos
-    cout<<"Digite a chave: "<<endl;
+    
+    vector<Node*> *filhos = new vector<Node*>;
+
+    cout<<endl<<"============================================================================"<<endl;
+    cout<<"Digite o valor da chave (se quiser nao quiser mais inserir digite -9999): "<<endl;
     cin>>N;
     //--------------
-    if(N==-100)
+    if(N==-9999)
       break;
     
-    cout<<"Digite o pai do no "<<endl;
+    cout<<"Digite o Pai do no "<<N<<":(para o primeiro caso digite 'raiz') "<<endl;
     cin>>P;
-    cout<<"Digite a qtd de filhos: "<<endl;
+    cout<<"Digite a quantidade de filhos do no "<<N<<": "<<endl;
     cin>>F;
     
-   while(F--){
-      int no = 0;
-      cin>>no;
-      Node *x = new Node(no,NULL);
-      filhos->push_back(x);
+    cout<<"Agora digite os valores das chaves dos nos: ";
+    while(F--){
+        int no = 0;
+        cin>>no;
+        Node *x = new Node(no,NULL);
+        filhos->push_back(x);
     }
+    cout<<endl<<"============================================================================"<<endl;
 
     Node *pai = new Node(N, filhos);
-    
+      
     if(P == "raiz"){
-      root = pai;
+        root = pai;
     }
     else{ //se nao for o 1º elemento
       int aux = stoi(P); //converte para inteiro a string referente ao pai
-      busca(root,pai,aux);
+      adiciona(root,pai,aux);
       flag = false;
     }
-
+    
   }
-  
-  pos_ordem(root);
+
+    
+  while(true){
+    int op = 0;
+    cout<<endl<<"============================================================================"<<endl;
+    cout<<"Selecione a opcao de impressao 1(pre ordem), 2(em ordem), 3(pos ordem) ou qualquer outro numero para sair: ";
+    cin>>op;
+    switch(op){
+      case 1:
+        pre_ordem(root);
+        cout<<endl<<"============================================================================"<<endl;
+        break;
+      case 2:
+        em_ordem(root);
+        cout<<endl<<"============================================================================"<<endl;
+        break;
+      case 3:
+        pos_ordem(root);
+        cout<<endl<<"============================================================================"<<endl;
+        break;
+      default:
+        return 0;
+    }
+  }
 
 	return 0;
 }
