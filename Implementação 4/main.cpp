9ca379350e20af5bce4bbfd3b146dad37f7e8605
Diego Luiz <51066402+Diego-Luiz@users.noreply.github.com>
2020-04-15 12:59:15 -0400

    Atualizado com mais comentarios

--- Implementação 4/main.cpp
+++ Implementação 4/main.cpp
@@ -1,17 +1,13 @@
 #include <iostream>
 #include <string>
-#include <stdio.h>
-#include <stdlib.h>
 #include <map>
-#include<string.h>
-#include<vector>
+#include <vector>
 
 using namespace std;
 
-int contador=0;//contador para verificar a ordem dos elementos e elementos repetidos
+/*------------------------------Classes----------------------------*/
 
-multimap<int, int> mapa_de_verificacao;//multimap para verificar a ordem de elementos repetidos
-
+//Classe que representa cada item (caracter e frequência)
 class caracter_num {
 	public :
 		char c;
@@ -22,30 +18,7 @@ class caracter_num {
 		}
 };
 
-vector <caracter_num> lista;
-
-void insere(char caracter, int num){
-	caracter_num novo(caracter, num);
-	contador += 1;
-	mapa_de_verificacao.insert(pair<int, int>(num, contador));
-	
-	
-	if(lista.size() == 0){
-		lista.push_back(novo);
-		return;
-	}
-	for(vector<caracter_num>:: iterator it=lista.begin(); it!=lista.end(); it++){
-		if(num < it->freq){
-			lista.insert(it, novo);
-			return;
-		}
-	}
-	
-	lista.push_back(novo);
-	
-}
-
-
+//Classe que representa os nós da "árvore"
 class No {
 	public:
 		caracter_num chave;
@@ -61,30 +34,60 @@ class No {
 	
 	
 };
+/*-----------------------------------------------------------------*/
 
 
+/*--------------------------------- Estrutura de dados usadas para conseguir realizar o programa -----------------------------*/
+//Lista dos caracteres e suas frequências
+vector <caracter_num> lista;
+//Árvore de Ruffman
 multimap <int, No*> pseudo_arvore;
+//Árvore auxiliar para a construção da árvore de Ruffman
 multimap <int, No*> pseudo_arvore_copia;
-
+//Raiz da árvore de Ruffman (pseudo_arvore)
 No *raiz = NULL;
+//Variavel booleana usada para ajudar na criação da representação binária dos caracteres
+bool achou = false;
+/*-----------------------------------------------------------------------------------------------------------------------------*/
 
+/*---------------------------------------------------------Funções-------------------------------------------------------------*/
+//Função que insere os caracteres e suas frequências na lista de forma ordenada
+void insere(char caracter, int num){ 
+	//criando o item a ser inserido
+	caracter_num novo(caracter, num);
+	//insere no começo
+	if(lista.size() == 0){
+		lista.push_back(novo);
+		return;
+	}
+	//insere de forma ordenada 
+	for(vector<caracter_num>:: iterator it=lista.begin(); it!=lista.end(); it++){
+		if(num < it->freq){
+			lista.insert(it, novo);
+			return;
+		}
+	}
+	//insere no final
+	lista.push_back(novo); 
+	
+}
 
+//Função que gera os nós da árvore
 void gerar_nos(){
 	
-	while(lista.size() > 1){
+	while(lista.size() > 1){ //Enquanto tiver elemento na lista para inserir na árvore
 		vector<caracter_num>:: iterator it = lista.begin();
 		int aux = 0;
 		if(it->c != '|'){
-			
 			it++;
 			if(it->c != '|'){
 				it--;
 				aux = it->freq;
-			No *esquerdo = new No(it->c, it->freq, NULL, NULL);
+				No *esquerdo = new No(it->c, it->freq, NULL, NULL);
 				
-			lista.erase(it);
+				lista.erase(it);
 			
-			it = lista.begin();
+				it = lista.begin();
 				No *direito = new No(it->c, it->freq, NULL, NULL);
 				aux += it->freq;
 				No *Pai = new No('|', aux, esquerdo, direito);
@@ -97,86 +100,83 @@ void gerar_nos(){
 				
 				insere('|', aux);
 				
-			}else{
+			}
+			else{
 				it--;
 				char c_anterior;
-			int freq_anterior;
-			if(it->c != '|'){//se o 1 nao for no
-				aux = it->freq;
-				c_anterior = it->c;
-				freq_anterior = it->freq;
-				
-				lista.erase(it);
-				
-				it = lista.begin();
-				
-				aux += it->freq;
-				if(it->c == '|'){//se o 2 for um no
-					
-					No *Pai;
-					
-					No *esquerda = new No(c_anterior, freq_anterior, NULL, NULL); 
-					
-					multimap<int, No*>::iterator mult_it = pseudo_arvore_copia.find(it->freq);
-					
-					if(mult_it != pseudo_arvore.end()){//-----------
-						Pai = new No('|', aux, esquerda, mult_it->second);
-						pseudo_arvore_copia.erase(mult_it);
-					}
-					
-					lista.erase(it);
-					
-					insere('|', aux);
-					
-					pseudo_arvore.insert(pair<int, No*>(aux, Pai));
-					pseudo_arvore_copia.insert(pair<int, No*>(aux, Pai));
-					
-				}
-			}
-			}
-		}
-		
-		else{
-			
-				if(it->c == '|'){//se o 1 for um no
-					No *esquerdo;
-					No *direito;
+				int freq_anterior;
+				if(it->c != '|'){//se o 1 nao for no
 					aux = it->freq;
-					
-					multimap<int, No*>::iterator mult_it = pseudo_arvore_copia.find(it->freq);
-					
-					if(mult_it != pseudo_arvore.end()){//---------
-						esquerdo = mult_it->second;
-						pseudo_arvore_copia.erase(mult_it);
-					}
+					c_anterior = it->c;
+					freq_anterior = it->freq;
 					
 					lista.erase(it);
 					
 					it = lista.begin();
 					
-					if(it->c != '|'){//o 1 eh no e o segundo nao eh no
-						direito = new No(it->c, it->freq, NULL, NULL);
+					aux += it->freq;
+					if(it->c == '|'){//se o 2 for um no
 						
-					}else{//os dois sao nos
+						No *Pai;
+						
+						No *esquerda = new No(c_anterior, freq_anterior, NULL, NULL); 
+						
 						multimap<int, No*>::iterator mult_it = pseudo_arvore_copia.find(it->freq);
-						if(mult_it != pseudo_arvore.end()){//------------
-							direito = mult_it->second;
+						
+						if(mult_it != pseudo_arvore.end()){//-----------
+							Pai = new No('|', aux, esquerda, mult_it->second);
 							pseudo_arvore_copia.erase(mult_it);
 						}
+						
+						lista.erase(it);
+						
+						insere('|', aux);
+						
+						pseudo_arvore.insert(pair<int, No*>(aux, Pai));
+						pseudo_arvore_copia.insert(pair<int, No*>(aux, Pai));
+						
 					}
-					
-					aux += it->freq;
-					
-					No *Pai = new No('|', aux, esquerdo, direito);
-					
-					lista.erase(it);
-					
-					insere('|', aux);
-					
-					pseudo_arvore.insert(pair<int, No*>(aux, Pai));
-					pseudo_arvore_copia.insert(pair<int, No*>(aux, Pai));
-					
 				}
+			}
+		}
+		
+		else{
+			
+			if(it->c == '|'){//se o 1 for um no
+				No *esquerdo;
+				No *direito;
+				aux = it->freq;
+					
+				multimap<int, No*>::iterator mult_it = pseudo_arvore_copia.find(it->freq);
+					
+				if(mult_it != pseudo_arvore.end()){//---------
+					esquerdo = mult_it->second;
+					pseudo_arvore_copia.erase(mult_it);
+				}
+					
+				lista.erase(it);
+					
+				it = lista.begin();
+					
+				if(it->c != '|'){//o 1 eh no e o segundo nao eh no
+					direito = new No(it->c, it->freq, NULL, NULL);	
+				}
+				else{//os dois sao nos
+					multimap<int, No*>::iterator mult_it = pseudo_arvore_copia.find(it->freq);
+					if(mult_it != pseudo_arvore.end()){//se achou o elemento
+						direito = mult_it->second;
+						pseudo_arvore_copia.erase(mult_it);
+					}
+				}
+					
+				aux += it->freq;
+				No *Pai = new No('|', aux, esquerdo, direito);
+				lista.erase(it);
+				insere('|', aux);
+				pseudo_arvore.insert(pair<int, No*>(aux, Pai));
+				pseudo_arvore_copia.insert(pair<int, No*>(aux, Pai));
+					
+			}
 			
 		}
 			
@@ -185,8 +185,7 @@ void gerar_nos(){
 }
 
 
-bool achou = false;
-
+//Função para criar a representação binária dos caracteres
 string criar_codigo(No *no, char caracter, string str, char direcao){
 	
 	if(no == NULL){
@@ -209,6 +208,8 @@ string criar_codigo(No *no, char caracter, string str, char direcao){
 		}
 	}
 }
+/*----------------------------------------------------------------------------------------------------------------------------*/
+
 
 int main(){
 	
@@ -220,13 +221,17 @@ int main(){
     multimap<int,char> freq_ordenado;
     multimap<int,char>::iterator itord;
     FILE *arq;
-    
-
+    string name_arch="";
+	cout<<"Digite o nome do arquivo a ser manipulado(sem a extensao .txt): "<<endl;
+	getline(cin,name_arch);
+	name_arch+=".txt";
+	cout<<"Nome do arquivo: "<<name_arch<<endl;
     //---ARQUIVO
-    arq = fopen("Arq.txt", "rt");
-    if(arq ==NULL)
+    arq = fopen(name_arch.c_str(), "rt");
+    if(arq ==NULL){
         cout<<"Erro! Arquivo nao encontrado!"<<endl;
-  
+		exit(1);
+	}
     char c;
     do{
         c = fgetc(arq);
@@ -240,11 +245,12 @@ int main(){
 	fclose(arq);
 	
     //MULTIMAP
+	
     for(itfreq=frequencia.begin();itfreq!=frequencia.end();itfreq++){
         freq_ordenado.insert( pair<int,char>(itfreq->second,itfreq->first) );
     }
 	
-	
+	cout<<"\nFrequencia do caracter : Caracter"<<endl;
     for(itord = freq_ordenado.begin(); itord!=freq_ordenado.end();itord++){
         cout<<itord->first<<" : "<<itord->second<<endl;
     }
@@ -259,6 +265,8 @@ int main(){
 		
 		if(str == "\n")
 			str = "newline";
+		if(str == " ")
+			str= "space";
 		str += "-" + to_string(itfreq->second) + "\n";
 		
 		fputs(str.c_str(), arq);
@@ -266,38 +274,32 @@ int main(){
 	
 	fclose(arq);
 	
-	//---------
-	for(itfreq=frequencia.begin(); itfreq != frequencia.end(); itfreq++){//populando a lista
+	//Preenchendo a lista, para conseguir construir a árvore 
+	for(itfreq=frequencia.begin(); itfreq != frequencia.end(); itfreq++){
 		insere(itfreq->first, itfreq->second);
 	}
 	
-	for(int i=0; i<lista.size(); i++){
-		cout<<"lista"<<endl;
-		cout<<lista[i].c<<" "<<lista[i].freq<<endl;
-	}
-	
+	//Gerando os nós da árvore
 	gerar_nos();
 		
-	
 	multimap<int, No*>:: iterator it = pseudo_arvore.begin();
 	
-	
+	cout<<"\nArvore gerada: "<<endl;
 	for(it; it!= pseudo_arvore.end(); it++){
-		cout<<it->first<<" : esquerda: "<<it->second->FE->chave.c<<", "<<it->second->FE->chave.freq<<" : direita: "<<it->second->FD->chave.c<<", "<<it->second->FD->chave.freq<<endl;
+		cout<<"No: "<<it->first<<" : Esquerda: "<<it->second->FE->chave.c<<", "<<it->second->FE->chave.freq<<" : Direita: "<<it->second->FD->chave.c<<", "<<it->second->FD->chave.freq<<endl;
 	}
 	
-	
-	it = pseudo_arvore.end();//definindo a raiz da arvore
+	/*--- Definindo a raiz da arvore*/
+	it = pseudo_arvore.end();
 	it--;
 	raiz = it->second;
-	
+	/*------------------------------*/
 	
 	arq = fopen("TABELADECODIGOS.txt", "wt");
 	for(itfreq=frequencia.begin(); itfreq != frequencia.end(); itfreq++){
 		
 		string codigo = criar_codigo(raiz, itfreq->first, "", 'n');
 		codigo = codigo.erase(codigo.size()-1);//tirando o n do final da string
-		cout<<codigo<<endl;
 		string invertida="";
 		
 		for(string::reverse_iterator rit=codigo.rbegin(); rit!=codigo.rend(); ++rit){
@@ -343,18 +345,15 @@ int main(){
 	
 
 	string deco="";
-	cout<<"antes do while"<<endl;
 	do{
         c = fgetc(arq);
 		
         deco += c;
-		cout<<"deco: "<<deco<<endl;
 		if(c != EOF){
 			
 			ittabela = tabeladecodigos_invertida.find(deco);
 			
 			if(ittabela != tabeladecodigos_invertida.end()){
-				cout<<ittabela->second<<endl;
 				fputc(ittabela->second, decodificado);
 				deco = "";
 			}
